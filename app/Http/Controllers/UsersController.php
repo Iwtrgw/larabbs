d08e67a835c726d1aecbc67a9adebb59e1a7b520
Iwtrgw <825179537@qq.com>
2018-08-23 14:27:59 +0000

    个人资料更新、页面、时间友好的显示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,11 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
 use App\Models\User;
 
 class UsersController extends Controller {
 	// 个人页面展示
 	public function show(User $user) {
 		return view('users.show', compact('user'));
+	}
+
+	// 个人资料编辑页面展示
+	public function edit(User $user) {
+		return view('users.edit', compact('user'));
+	}
+
+	// 个人资料更新
+	public function update(UserRequest $request, User $user) {
+		$user->update($request->all());
+
+		return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
 	}
 }

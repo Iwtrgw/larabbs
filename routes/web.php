e081184e6ac39ef2e0985263265fe8d4aa36562d
Iwtrgw <825179537@qq.com>
2018-08-27 12:31:26 +0000

    分类下的话题列表

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,6 @@ Route::post('password/reset', 'Auth\ForgotPasswordController@reset');
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类列表话题
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);

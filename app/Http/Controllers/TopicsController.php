23dc9c451308a22fbd76b1e5d0de827d4cea41ec
Iwtrgw <825179537@qq.com>
2018-09-02 02:58:57 +0000

    文章展示页面的布局样式优化

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -35,7 +35,7 @@ class TopicsController extends Controller {
 		$topic->user_id = Auth::id();
 		$topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题！');
 	}
 
 	public function edit(Topic $topic) {
@@ -47,14 +47,14 @@ class TopicsController extends Controller {
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功！');
 	}
 
 	public function destroy(Topic $topic) {
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '成功删除！');
 	}
 
 	/* 图片上传处理 */

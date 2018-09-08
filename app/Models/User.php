6046cfe91f6f1146493f4bd0a2d81069547dcd6d
Iwtrgw <825179537@qq.com>
2018-09-08 09:30:50 +0000

    添加消息通知

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,11 +2,24 @@
 
 namespace App\Models;
 
+use Auth;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable {
-	use Notifiable;
+	use Notifiable {
+		notify as protected laravelNotify;
+	}
+
+	public function notify($instance) {
+		// 如果要通知的人是当前用户，就不必通知了！
+		if ($this->id == Auth::id()) {
+			return;
+		}
+
+		$this->increment('notification_count');
+		$this->laravelNotify($instance);
+	}
 
 	/**
 	 * The attributes that are mass assignable.
@@ -37,5 +50,12 @@ class User extends Authenticatable {
 	// 关联回复表
 	public function replies() {
 		return $this->hasMany(Reply::class);
+	}
+
+	// 清空未读消息标示
+	public function markAsRead() {
+		$this->notification_count = 0;
+		$this->save();
+		$this->unreadNotifications->markAsRead();
 	}
 }

365c83bfb316c3cd0d360493a29a90d0006f8c75
IWTRGW\Iwtrgw <iwtrgw@outlook.com>
2021-08-16 21:37:15 +0800

    添加保存方法

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -8,8 +8,21 @@ use App\Models\Topic;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+/**
+ * Class CategoriesController
+ * @package App\Http\Controllers
+ */
 class CategoriesController extends Controller {
-	public function show(Category $category, Request $request, Topic $topic, User $user, Link $link) {
+
+    /**
+     * @param Category $category
+     * @param Request $request
+     * @param Topic $topic
+     * @param User $user
+     * @param Link $link
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function show(Category $category, Request $request, Topic $topic, User $user, Link $link) {
 		// 读取分类 ID 关联的话题，并按每 20 条分页
 		$topics = $topic->withOrder($request->order)
 			->where('category_id', $category->id)
@@ -23,5 +36,14 @@ class CategoriesController extends Controller {
 
 		// 传参变量话题和分类到模板中
 		return view('topics.index', compact('topics', 'category', 'active_users', 'links'));
+	}
+
+    /**
+     *  保存
+     * @param Request $request
+     */
+    public function save(Request $request)
+    {
+        // TODO
 	}
 }

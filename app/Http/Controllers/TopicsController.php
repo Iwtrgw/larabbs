97f9a9d12b0550d379f239526647f7ad0d644ade
IWTRGW\Iwtrgw <iwtrgw@outlook.com>
2021-09-30 12:10:04 +0800

    小调整

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -3,98 +3,107 @@
 namespace App\Http\Controllers;
 
 use App\Handlers\ImageUploadHandler;
-use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use App\Models\Link;
 use App\Models\Topic;
 use App\Models\User;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
-class TopicsController extends Controller {
-	public function __construct() {
-		$this->middleware('auth', ['except' => ['index', 'show']]);
-	}
+class TopicsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
 
-	public function index(TopicRequest $request, Topic $topic, User $user, Link $link) {
+    public function index(TopicRequest $request, Topic $topic, User $user, Link $link)
+    {
 
-		$topics = $topic->withOrder($request->order)->paginate(20);
-		$active_users = $user->getActiveUsers(); // 取出活跃用户
-		$links = $link->getAllCached(); // 取出侧边栏推荐的资源
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        $active_users = $user->getActiveUsers(); // 取出活跃用户
+        $links = $link->getAllCached(); // 取出侧边栏推荐的资源
 
-		return view('topics.index', compact('topics', 'active_users', 'links'));
-	}
+        return view('topics.index', compact('topics', 'active_users', 'links'));
+    }
 
-	public function show(Request $request, Topic $topic) {
+    public function show(Request $request, Topic $topic)
+    {
 
-		// URL 矫正
-		if (!empty($topic->slug) && $topic->slug != $request->slug) {
-			return redirect($topic->link(), 301);
-		}
-		return view('topics.show', compact('topic'));
-	}
+        // URL 矫正
+        if (!empty($topic->slug) && $topic->slug != $request->slug) {
+            return redirect($topic->link(), 301);
+        }
+        return view('topics.show', compact('topic'));
+    }
 
-	public function create(Topic $topic) {
+    public function create(Topic $topic)
+    {
 
-		$categories = Category::all();
-		return view('topics.create_and_edit', compact('topic', 'categories'));
-	}
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request, Topic $topic) {
-		$topic->fill($request->all());
-		$topic->user_id = Auth::id();
-		$topic->save();
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
 
-		return redirect()->to($topic->link())->with('message', '成功创建话题！');
-	}
+        return redirect()->to($topic->link())->with('message', '成功创建话题！');
+    }
 
-	public function edit(Topic $topic) {
-		$this->authorize('update', $topic);
+    public function edit(Topic $topic)
+    {
+        $this->authorize('update', $topic);
 
-		$categories = Category::all();
+        $categories = Category::all();
 
-		return view('topics.create_and_edit', compact('topic', 'categories'));
-	}
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic) {
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->to($topic->link())->with('message', '更新成功！');
-	}
+        return redirect()->to($topic->link())->with('message', '更新成功！');
+    }
 
-	public function destroy(Topic $topic) {
-		$this->authorize('destroy', $topic);
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
 
-		$topic->delete();
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('message', '成功删除！');
-	}
+        return redirect()->route('topics.index')->with('message', '成功删除！');
+    }
 
-	/* 图片上传处理 */
-	public function uploadImage(Request $request, ImageUploadHandler $uploader) {
+    /* 图片上传处理 */
+    public function uploadImage(Request $request, ImageUploadHandler $uploader)
+    {
 
-		// 初始休返回数据 ，默认是失败的
-		$data = [
-			'success' => false,
-			'msg' => '上传失败！',
-			'file_path' => '',
-		];
+        // 初始休返回数据 ，默认是失败的
+        $data = [
+            'success' => false,
+            'msg' => '上传失败！',
+            'file_path' => '',
+        ];
 
-		// 判断是否有上传文件，并赋值给 $file
-		if ($file = $request->upload_file) {
-			// 保存图片到本地
-			$result = $uploader->save($request->upload_file, 'topics', Auth::id(), 1024);
-			// 图片保存成功的处理
-			if ($result) {
-				$data['file_path'] = $result['path'];
-				$data['msg'] = "上传成功！";
-				$data['success'] = true;
-			}
-		}
+        // 判断是否有上传文件，并赋值给 $file
+        if ($file = $request->upload_file) {
+            // 保存图片到本地
+            $result = $uploader->save($request->upload_file, 'topics', Auth::id(), 1024);
+            // 图片保存成功的处理
+            if ($result) {
+                $data['file_path'] = $result['path'];
+                $data['msg'] = "上传成功！";
+                $data['success'] = true;
+            }
+        }
 
-		return $data;
+        return $data;
 
-	}
+    }
 }

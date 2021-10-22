0fb47a236477644856dda51e3881df03f287aed5
Iwtrgw <825179537@qq.com>
2021-10-23 02:57:06 +0800

    站点配置

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -4,6 +4,6 @@
             由 <a href="https://weibo.com/u/3196302477?is_all=1" target="_blank">Iwtrgw</a> 刻意练习, 每日精进 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="mailto:Iwtrgw@gmail.com">联系我</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我</a></p>
     </div>
 </footer>

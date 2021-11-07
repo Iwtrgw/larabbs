0c3561a023bbda5f6a40f8f1955b287e46f70571
Iwtrgw <825179537@qq.com>
2021-11-08 03:10:52 +0800

    邮件通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -4,10 +4,11 @@ namespace App\Notifications;
 
 use App\Models\Reply;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class TopicReplied extends Notification {
+class TopicReplied extends Notification implements ShouldQueue {
 	use Queueable;
 
 	public $reply;
@@ -30,7 +31,9 @@ class TopicReplied extends Notification {
 	 * @return array
 	 */
 	public function via($notifiable) {
-		return ['database'];
+
+		// 开启通知的频道
+		return ['database', 'mail'];
 	}
 
 	/**
@@ -40,10 +43,12 @@ class TopicReplied extends Notification {
 	 * @return \Illuminate\Notifications\Messages\MailMessage
 	 */
 	public function toMail($notifiable) {
+		$url = $this->reply->topic->link(['#reply' . $this->reply->id]);
+
 		return (new MailMessage)
-			->line('The introduction to the notification.')
-			->action('Notification Action', url('/'))
-			->line('Thank you for using our application!');
+			->line('你的话题有新回复！')
+			->action('查看回复', $url);
+
 	}
 
 	/**

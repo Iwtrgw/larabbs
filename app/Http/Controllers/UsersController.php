443f2e78c8247806b6793192b49124ed76eadd67
Iwtrgw <825179537@qq.com>
2021-12-07 14:00:26 +0800

    添加个人展示页面

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+
+class UsersController extends Controller {
+	// 个人页面展示
+	public function show(User $user) {
+		return view('users.show', compact('user'));
+	}
+}

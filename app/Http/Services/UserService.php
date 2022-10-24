271b08de175fc62f2d17b865133cac3b26693780
iwtrgw <iwtrgw@gmail.com>
2022-10-24 21:57:33 +0800

    doc.....

--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -4,18 +4,33 @@ namespace App\Http\Services;
 
 use App\Models\User;
 
+/**
+ *
+ */
 class UserService
 {
+    /**
+     * @param User $user
+     * @return void
+     */
     public function save(User $user)
     {
 
     }
 
+    /**
+     * @param User $user
+     * @return void
+     */
     public function edit(User $user)
     {
 
     }
 
+    /**
+     * @param $uid
+     * @return void
+     */
     public function del($uid)
     {
 

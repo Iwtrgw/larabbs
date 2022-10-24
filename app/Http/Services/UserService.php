59ee7490fd10a521bfb1b844a8390345d10fa687
iwtrgw <iwtrgw@gmail.com>
2022-10-24 21:53:42 +0800

    use usermodel

--- app/Http/Services/UserService.php
+++ app/Http/Services/UserService.php
@@ -2,20 +2,22 @@
 
 namespace App\Http\Services;
 
+use App\Models\User;
+
 class UserService
 {
-    public function save($user)
+    public function save(User $user)
     {
-        
+
     }
 
-    public function edit($user)
+    public function edit(User $user)
     {
-        
+
     }
 
     public function del($uid)
     {
-        
+
     }
 }

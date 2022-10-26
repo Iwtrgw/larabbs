e8572acb47a4187df8b236539fadd33fdb63a2d6
iwtrgw <iwtrgw@gmail.com>
2022-10-26 19:48:54 +0800

    Topic user 添加约束

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -37,7 +37,7 @@ class Topic extends Model
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function user()
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class);
     }

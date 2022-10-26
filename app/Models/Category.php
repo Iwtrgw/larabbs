a714844f2044b2ae9b90ffcd6e57735cd9cae60b
iwtrgw <iwtrgw@gmail.com>
2022-10-26 19:37:02 +0800

    category 增加注释

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Category Model
+ */
 class Category extends Model
 {
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name', 'description',
     ];

53e701a85860159c379805b51b4eb076df5633f6
iwtrgw <iwtrgw@gmail.com>
2022-10-26 19:41:46 +0800

    Topic 格式化

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -5,85 +5,93 @@ namespace App\Models;
 /**
  *
  */
-class Topic extends Model {
+class Topic extends Model
+{
     /**
      * @var string[]
      */
     protected $fillable = ['title', 'body', 'category_id', 'excerpt', 'slug'];
 
-	// 关联回复表
+    // 关联回复表
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
-    public function replies() {
-		return $this->hasMany(Reply::class);
-	}
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 
-	// 关联分类表
-
-    /**
-     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
-     */
-    public function category() {
-		return $this->belongsTo(Category::class);
-	}
-
-	// 关联用户表
+    // 关联分类表
 
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function user() {
-		return $this->belongsTo(User::class);
-	}
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 
-	// 排序
+    // 关联用户表
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // 排序
 
     /**
      * @param $query
      * @param $order
      * @return mixed
      */
-    public function scopeWithOrder($query, $order) {
-		// 不同的排序，使用不同的数据读取逻辑
-		switch ($order) {
-		case 'recent':
-			$query->recent();
-			break;
+    public function scopeWithOrder($query, $order)
+    {
+        // 不同的排序，使用不同的数据读取逻辑
+        switch ($order) {
+            case 'recent':
+                $query->recent();
+                break;
 
-		default:
-			$query->recentReplied();
-			break;
-		}
+            default:
+                $query->recentReplied();
+                break;
+        }
 
-		// 预加载防止 N+1 问题
-		return $query->with('user', 'category');
-	}
-
-    /**
-     * @param $query
-     * @return mixed
-     */
-    public function scopeRecentReplied($query) {
-		// 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
-		// 此时会自动触发框架对数据模型 updated_at 时间戳的更新
-		return $query->orderBy('updated_at', 'desc');
-	}
+        // 预加载防止 N+1 问题
+        return $query->with('user', 'category');
+    }
 
     /**
      * @param $query
      * @return mixed
      */
-    public function scopeRecent($query) {
-		return $query->orderBy('created_at', 'desc');
-	}
+    public function scopeRecentReplied($query)
+    {
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     */
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 
     /**
      * @param $params
      * @return string
      */
-    public function link($params = []) {
-		return route('topics.show', array_merge([$this->id, $this->slug], $params));
-	}
+    public function link($params = [])
+    {
+        return route('topics.show', array_merge([$this->id, $this->slug], $params));
+    }
 }

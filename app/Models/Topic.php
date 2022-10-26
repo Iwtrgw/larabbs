06bc3ad5d3458b1e4923193bb00335392fbc004a
iwtrgw <iwtrgw@gmail.com>
2022-10-26 19:44:01 +0800

    Topic category 添加约束

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -27,7 +27,7 @@ class Topic extends Model
     /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
-    public function category()
+    public function category(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(Category::class);
     }

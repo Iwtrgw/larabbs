22dc0776a8d478576885b97491068a74f0ca8d3f
iwtrgw <iwtrgw@gmail.com>
2022-10-27 20:35:20 +0800

    User isAuthorOf 添加约束

--- app/Models/User.php
+++ app/Models/User.php
@@ -64,7 +64,11 @@ class User extends Authenticatable {
 		return $this->hasMany(Topic::class);
 	}
 
-	public function isAuthorOf($model): bool
+    /**
+     * @param $model
+     * @return bool
+     */
+    public function isAuthorOf($model): bool
     {
 		return $this->id == $model->user_id;
 	}

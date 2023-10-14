1c61fd3dc25625a3363769495f2f5bb8cc2bee05
iwtrgw <iwtrgw@gmail.com>
2023-10-14 11:24:52 +0800

    page组件初始化

--- resources/assets/js/components/pageComponent.vue
+++ resources/assets/js/components/pageComponent.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">内容组件</div>
+                    <div class="panel-heading">Page</div>
 
                     <div class="panel-body">
                         {{ content.context }}

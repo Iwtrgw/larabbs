7ef9693a6c59f71a1a07d2ca5ebe901a23863f40
iwtrgw <iwtrgw@gmail.com>
2023-10-16 21:18:00 +0800

    update about controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -44,7 +44,6 @@ class AboutController extends Controller
     /**
      *  个人资料更新
      * @param User $user
-     * @throws \Illuminate\Auth\Access\AuthorizationException
      */
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user): \Illuminate\Http\RedirectResponse
     {

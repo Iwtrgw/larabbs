5848c00a3f81c842ab7814b850156e2886538848
iwtrgw <iwtrgw@gmail.com>
2023-10-16 21:19:21 +0800

    update about controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -55,7 +55,6 @@ class AboutController extends Controller
         }
 
         $user->update($data);
-
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }

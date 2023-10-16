f487526cd8c6e9847b030be0cb186215244786d6
iwtrgw <iwtrgw@gmail.com>
2023-10-16 21:20:39 +0800

    update about controller

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -47,7 +47,6 @@ class AboutController extends Controller
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user): \Illuminate\Http\RedirectResponse
     {
 
-
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }

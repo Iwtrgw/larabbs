3557c232ccff821c0c3e3ccd860c23920d7cd491
iwtrgw <iwtrgw@gmail.com>
2023-10-23 21:47:48 +0800

    add orderstatus enum file

---
+++ Admin/Enums/OrderStatus.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Enums;
+
+
+/**
+ * 通用状态说明
+ * Class PaymentType
+ * @package App\Enums
+ */
+class CommonStatus extends Enums
+{
+
+    const USABLE  = 0;
+    const DISABLE = 1;
+
+    public static $translations = [
+        self::USABLE  => '可用',
+        self::DISABLE => '禁用',
+    ];
+
+    /**
+     * 检查是否可用
+     * @param $status
+     * @return bool
+     */
+    public static function checkUsable($status)
+    {
+        return $status == CommonStatus::USABLE;
+    }
+}

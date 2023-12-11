1071d1245321177b5e673a9f917434b73825ed1f
iwtrgw <iwtrgw@gmail.com>
2023-12-11 21:18:26 +0800

    ReplyPolicy

--- Admin/Policies/ReplyPolicy.php
+++ Admin/Policies/ReplyPolicy.php
@@ -5,6 +5,9 @@ namespace App\Policies;
 use App\Models\Reply;
 use App\Models\User;
 
+/**
+ * ReplyPolicy
+ */
 class ReplyPolicy extends Policy {
 	public function update(User $user, Reply $reply) {
 		// return $reply->user_id == $user->id;

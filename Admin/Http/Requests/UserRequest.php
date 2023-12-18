1cb79f92eba5751ff5ecbde12d30822777a64286
iwtrgw <iwtrgw@gmail.com>
2023-12-18 21:06:12 +0800

    UserRequest

--- Admin/Http/Requests/UserRequest.php
+++ Admin/Http/Requests/UserRequest.php
@@ -15,7 +15,7 @@ class UserRequest extends FormRequest
      *
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
